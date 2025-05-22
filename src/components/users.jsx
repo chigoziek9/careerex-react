1f918b43c209de42b739c2b4ac6799fdd132cc49
Nony <nonybrighto@gmail.com>
2025-05-22 14:04:09 +0100

    number commit

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 function RandomUser() {
-  const [user, setUser] = useState(null);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/")
-      .then(res => res.json())
-      .then(data => {
-        setUser(data.results[0]);
+    fetch("https://randomuser.me/api/?results=5")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.results);
       });
   }, []);
 
-  if (!user) return <p class="flex justify-center items-center h-screen text-4xl font-bold ">Loading...</p>;
+  if (users.length === 0)
+    return (
+      <p className="flex justify-center items-center h-screen text-4xl font-bold ">
+        Loading...
+      </p>
+    );
 
   return (
-    <div class="flex justify-center items-center h-screen bg-gray-100">
-      <img src={user.picture.large} alt="User" />
-      <ul class="ml-4 text-xl">
-        <li>NAME: {user.name.first} {user.name.last} </li>
-        <li>CITY: {user.location?.city && `, ${user.location.city}`} </li>
-        <li>EMAIL: {user.email}</li>
-        <li>DATE OF BIRTH: {user.dob?.date && `, ${new Date(user.dob.date).toLocaleDateString()}`}</li>
-        <li>PHONE NUMBER: {user.phone}</li>
-      </ul>
-      
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      {users.map((user, index) => (
+        <div key={index}>
+          <img src={user.picture.large} alt="User" />
+          <ul className="ml-4 text-xl">
+            <li>
+              NAME: {user.name.first} {user.name.last}{" "}
+            </li>
+            <li>CITY: {user.location?.city}</li>
+            <li>EMAIL: {user.email}</li>
+            <li>
+              DATE OF BIRTH:{" "}
+              {user.dob?.date && new Date(user.dob.date).toLocaleDateString()}
+            </li>
+            <li>PHONE NUMBER: {user.phone}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
